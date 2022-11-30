b023ab71a62f9d9bc04dcdc2990e10eec7d70cff
Shiva <dasshivashish11@gmail.com>
2022-11-30 23:11:25 +0530

    Working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate rustop;
+mod stdin;
+
+use rustop::opts;
+use stdin::StdinReader;
+
 fn main() {
-    println!("Hello, world!");
+    let (args, _) = opts!{
+        synopsis "A simple programming language";
+        version "0.0.1";
+        param file:Option<String>, desc:"Input file (will use stdin if not specified).";
+    }.parse_or_exit();
+
+   match args.file {
+       Some(s) => unimplemented!(),
+       None => {
+           let mut reader = StdinReader::new();
+           let mut s = String::new();
+           loop {
+               reader.read_line(&mut s);
+               println!("{}", s);
+           }
+       }
+   }
 }
---
+++ src/stdin.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+pub struct StdinReader {
+    line: u32
+}
+
+impl StdinReader {
+    pub fn new() -> Self {
+        StdinReader {
+            line: 0u32
+        }
+    }
+
+    pub fn read_line(&mut self, dest: &mut String) {
+        let line = io::stdin().read_line(dest);
+        match line {
+            Ok(s) => {
+                if s == 0 {
+                    std::process::exit(0);
+                }
+
+                let trim = dest.trim_end_matches("\n").len();
+                dest.truncate(trim);
+                self.line += 1;
+            }
+
+            Err(e) => {
+                panic!("{}", e);
+            }
+        }
+    }
+}
